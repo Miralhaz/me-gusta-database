99626018909aa7ce78c2f57a5ee8169c9a012618
enrico <enrico.faraldo@sptech.school>
2026-03-29 16:53:35 -0300

    atualizando o banco de dados, adicionando o script e regras

---
+++ SQL/megustaV01.sql
@@ -0,0 +1,97 @@
+CREATE DATABASE IF NOT EXISTS megusta;
+USE megusta;
+
+CREATE TABLE IF NOT EXISTS categoria (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS unidade_medida (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	unidade VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS tipo_status (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(30)
+);
+
+CREATE TABLE IF NOT EXISTS fornecedor (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(150),
+    cnpj VARCHAR(20),
+    telefone VARCHAR(20),
+    ativo BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(70),
+    senha VARCHAR(20),
+    email VARCHAR(70)
+);
+
+CREATE TABLE IF NOT EXISTS fogazza (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(70),
+    preco DECIMAL(19,4)
+);
+
+CREATE TABLE IF NOT EXISTS insumo (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    fk_categoria INT, 
+    fk_unidade_medida INT, 
+    fk_status INT, 
+    nome VARCHAR(70), 
+    codigo_insumo VARCHAR(15), 
+    estoque_minimo DECIMAL(10,2), 
+    qtd_atual DECIMAL(10,2), 
+    ativo BOOLEAN, 
+    dt_cadastro DATETIME,
+    FOREIGN KEY (fk_categoria) REFERENCES categoria(id),
+    FOREIGN KEY (fk_unidade_medida) REFERENCES unidade_medida(id),
+    FOREIGN KEY (fk_status) REFERENCES tipo_status(id)
+);
+
+CREATE TABLE IF NOT EXISTS compras_estoque (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    fk_fornecedor INT, 
+    fk_status INT, 
+    dt_pedido DATE, 
+    vl_total DECIMAL(19,4),
+    FOREIGN KEY (fk_fornecedor) REFERENCES fornecedor(id),
+    FOREIGN KEY (fk_status) REFERENCES tipo_status(id)
+);
+
+CREATE TABLE IF NOT EXISTS entrada_estoque (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    fk_insumo INT, 
+    fk_usuario INT, 
+    fk_compra INT, 
+    quantidade DECIMAL(10,2), 
+    dt_entrada DATETIME, 
+    lote VARCHAR(50), 
+    dt_validade DATE,
+    FOREIGN KEY (fk_insumo) REFERENCES insumo(id),
+    FOREIGN KEY (fk_usuario) REFERENCES usuario(id),
+    FOREIGN KEY (fk_compra) REFERENCES compras_estoque(id)
+);
+
+CREATE TABLE IF NOT EXISTS saida_estoque (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    fk_insumo INT, 
+    fk_usuario INT, 
+    quantidade DECIMAL(10,2), 
+    dt_saida DATETIME,
+    FOREIGN KEY (fk_insumo) REFERENCES insumo(id),
+    FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+CREATE TABLE fogazza_insumo (
+	fk_fogazza INT, 
+    fk_insumo INT, 
+    quantidade_insumo DECIMAL(10,2),
+    PRIMARY KEY (fk_fogazza, fk_insumo),
+    FOREIGN KEY (fk_fogazza) REFERENCES fogazza(id),
+    FOREIGN KEY (fk_insumo) REFERENCES insumo(id)
+);
