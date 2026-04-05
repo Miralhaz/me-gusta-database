80af9467941dbcbf5a560fd4bf8931d59a306279
Breno-0 <breno.costa@sptech.school>
2026-04-05 04:05:05 -0300

    adicionando usuario megusta_admin nobanco

---
+++ SQL/megustaV03.sql
@@ -0,0 +1,110 @@
+CREATE DATABASE IF NOT EXISTS megusta;
+USE megusta;
+
+CREATE TABLE IF NOT EXISTS categoria_insumo (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS categoria_fogazza (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS unidade_medida (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	unidade VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS tipo_status (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(30)
+);
+
+CREATE TABLE IF NOT EXISTS fornecedor (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(150),
+    cnpj VARCHAR(20),
+    telefone VARCHAR(20),
+    ativo BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(70),
+    senha VARCHAR(255),
+    email VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS fogazza (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(70),
+    preco DECIMAL(19,4),
+    fk_categoria_fogazza INT,
+    FOREIGN KEY (fk_categoria_fogazza) REFERENCES categoria_fogazza(id)
+);
+
+CREATE TABLE IF NOT EXISTS insumo (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    fk_categoria_insumo INT, 
+    fk_unidade_medida INT, 
+    fk_status INT, 
+    nome VARCHAR(70), 
+    codigo_insumo VARCHAR(15), 
+    estoque_minimo DECIMAL(10,2), 
+    qtd_atual DECIMAL(10,2), 
+    ativo BOOLEAN, 
+    dt_cadastro DATETIME,
+    FOREIGN KEY (fk_categoria_insumo) REFERENCES categoria_insumo(id),
+    FOREIGN KEY (fk_unidade_medida) REFERENCES unidade_medida(id),
+    FOREIGN KEY (fk_status) REFERENCES tipo_status(id)
+);
+
+CREATE TABLE IF NOT EXISTS compras_estoque (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    fk_fornecedor INT, 
+    fk_status INT, 
+    fk_insumo INT, 
+    dt_pedido DATE, 
+    vl_total DECIMAL(19,4),
+    FOREIGN KEY (fk_fornecedor) REFERENCES fornecedor(id),
+    FOREIGN KEY (fk_status) REFERENCES tipo_status(id),
+    FOREIGN KEY (fk_insumo) REFERENCES insumo(id)
+);
+
+CREATE TABLE IF NOT EXISTS entrada_estoque (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    fk_insumo INT, 
+    fk_usuario INT, 
+    fk_compra INT, 
+    quantidade DECIMAL(10,2), 
+    dt_entrada DATETIME, 
+    lote VARCHAR(50), 
+    dt_validade DATE,
+    FOREIGN KEY (fk_insumo) REFERENCES insumo(id),
+    FOREIGN KEY (fk_usuario) REFERENCES usuario(id),
+    FOREIGN KEY (fk_compra) REFERENCES compras_estoque(id)
+);
+
+CREATE TABLE IF NOT EXISTS saida_estoque (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    fk_insumo INT, 
+    fk_usuario INT, 
+    quantidade DECIMAL(10,2), 
+    dt_saida DATETIME,
+    FOREIGN KEY (fk_insumo) REFERENCES insumo(id),
+    FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+CREATE TABLE fogazza_insumo (
+	fk_fogazza INT, 
+    fk_insumo INT, 
+    quantidade_insumo DECIMAL(10,2),
+    PRIMARY KEY (fk_fogazza, fk_insumo),
+    FOREIGN KEY (fk_fogazza) REFERENCES fogazza(id),
+    FOREIGN KEY (fk_insumo) REFERENCES insumo(id)
+);
+
+CREATE USER 'megusta_admin'@'%' IDENTIFIED BY 'sptech';
+GRANT ALL PRIVILEGES ON megusta.* TO 'megusta_admin'@'%';
+FLUSH PRIVILEGES;
